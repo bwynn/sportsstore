3a81809d98fd37d96844b187b4ea439719ec7190
Brian Wynn <brian.wynn83@gmail.com>
2015-04-19 22:52:23 -0700

    Typo in html element caused login page to not call login controller.

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,7 +1,8 @@
-angular.modue("sportsStoreAdmin")
+angular.module("sportsStoreAdmin")
 .constant("authUrl", "http://localhost:5500/users/login")
 .controller("authCtrl", function($scope, $http, $location, authUrl) {
-  $scope.authenticate = function(user, pass) {
+
+  $scope.authenticate = function (user, pass) {
     $http.post(authUrl, {
       username: user,
       password: pass
